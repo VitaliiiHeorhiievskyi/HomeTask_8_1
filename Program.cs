6b561d0c5bf7e3a46e69087a94205ac17f61bf53
VitaliiiHeorhiievskyi <georgiievsky@gmail.com>
2021-10-23 14:28:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using HomeTask2;
+using System;
+
+namespace HomeTask_8_1
+{
+    public delegate int CompareToDelegate(object value1, object value2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrStr = new string[] { "Apple","Lemon","Banana" };
+
+            Sort.BubbleSort(arrStr, (value1, value2) => (((string)value1).CompareTo((string)value2)));//1 приклад
+
+            foreach (var item in arrStr)
+            {
+                Console.WriteLine(item);
+            }
+
+            var products = new Product[]
+            {
+               new Product("Apple",123,1000,22,DateTime.Now),
+               new Product("Lemon",21,500,233,DateTime.Now.AddDays(-15)),
+               new Product("Banana",14,32,53,DateTime.Now.AddDays(-10))
+            };
+
+            Sort.BubbleSort(products, (value1, value2) => (((Product)value1).CompareTo((Product)value2)));//2 приклад
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+
+
+}
---
+++ Sort.cs
@@ -0,0 +1,29 @@
+﻿using HomeTask2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_8_1
+{
+    static class Sort
+    {
+        public static void BubbleSort(object[] arr, CompareToDelegate compare)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (compare(arr[j], arr[j + 1]) == 1)
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
